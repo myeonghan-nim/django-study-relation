fef3605901fdd8b19b46f28cd218065d2fb30629
myeonghan-nim <ataraxia9408@gmail.com>
2021-10-02 21:31:48 +0900

    Change: explanatory notes

--- facebook/urls.py
+++ facebook/urls.py
@@ -1,4 +1,4 @@
-"""facebook URL Configuration
+'''facebook URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
@@ -12,13 +12,13 @@ Class-based views
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+'''
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
+
     path('posts/', include('posts.urls')),
     path('accounts/', include('accounts.urls')),
 ]
